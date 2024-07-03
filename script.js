418febcc85d543183a2b94ae00d539ce7c25fd8f
Xun <xun.li@student.maastrichtuniversity.nl>
2024-07-04 00:44:49 +0200

    up

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 window.addEventListener("load", function(){
-    showtest.innerHTML="load page";
+    var showtest = document.getElementById("showtest");
+    if (showtest) {
+        showtest.innerHTML="load page";
+    }
+
+    var button = document.getElementById("printbutton");
+    // Adding an event listener to the button that executes a function when the button is clicked
+    button.addEventListener("click", function() {
+        // Displaying an alert message
+        alert("You clicked the button!");
+        printSpecificContent();
+    });
+
+    var itemclicked = document.getElementById("activejob1");
+    itemclicked.addEventListener("click", function() {
+        loaddetail("101");
+    });
+
 });
-var button = document.getElementsByTagName("button")[0];
-// Adding an event listener to the button that executes a function when the button is clicked
-button.addEventListener("click", function() {
-    // Displaying an alert message
-    alert("You clicked the button!");
-    printSpecificContent();
-});
+
 
 function printSpecificContent() {
     var printContents = document.getElementById("printframe").innerHTML;
@@ -15,4 +26,25 @@ function printSpecificContent() {
     document.body.innerHTML = printContents;
     window.print();
     document.body.innerHTML = originalContents; // Restore original content
+}
+function loaddetail(clickeditem){
+    var itemdetail = document.getElementById("itemdetail");
+    var detailform=document.createElement("form");
+    var input1=document.createElement("input");
+    var input1label=document.createElement("label");
+    input1.type="text";
+    input1.name="jobid";
+    input1.value=clickeditem;
+    input1label.innerHTML="Job ID";
+    detailform.appendChild(input1label);
+    detailform.appendChild(input1);
+    itemdetail.appendChild(detailform);
+    var submitbutton=document.createElement("button");
+    submitbutton.innerHTML="Submit";
+    submitbutton.addEventListener("click", function(){
+        alert("You clicked the submit button!");
+    });
+    detailform.appendChild(submitbutton);
+    
+
 }
