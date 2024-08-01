6ef3296d0903190ff38310a579311b8482f1085c
Xun <xun.li@student.maastrichtuniversity.nl>
2024-08-01 23:50:15 +0200

    up

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function(){
     
     access=-1;
     sysresponse = document.getElementById("response");
-    sysresponse.innerHTML="欢迎";
+    sysresponse.innerHTML="欢迎。如遇版本更新，请按键盘Shift+F5刷新页面。";
     
     //page fist load
     // var searchcreteria = new FormData();
