6e48908ec91cce84c150fb2b4e87b1e56af054c0
Xun <xun.li@student.maastrichtuniversity.nl>
2024-08-09 03:29:57 +0200

    up

--- script.js
+++ script.js
@@ -500,7 +500,11 @@ function addnewjob(clickeditem,detaillinenumber){
     if(clickeditem==""){
         jobid = new Date().getTime();
     }else{
-        jobid = clickeditem['jobid'];
+        if (clickeditem['jobid'] == '') {
+            jobid = new Date().getTime();
+        }else{
+            jobid = clickeditem['jobid'];
+        }
     }
     addjob.append("jobid",jobid);
 
