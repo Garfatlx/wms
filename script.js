db65c9c63afd2ed63dadd7f68e1351308d4429a2
Xun <xun.li@student.maastrichtuniversity.nl>
2024-08-12 14:26:33 +0200

    up

--- script.js
+++ script.js
@@ -1174,7 +1174,7 @@ async function loaddetail(clickeditem,activity){
                         xlskgs = (!json[i]['kgs'])?xlskgs:xlskgs+Number(json[i]['kgs']);
                         xlsnote = (!json[i]['note'])?xlsnote:xlsnote+json[i]['note'] + ";";    
                         //if(!json[i+1]['label'] || json[i]['label']!=json[i+1]['label'] || (!json[i]['marks'] && json[i]['marks']!=json[i+1]['marks'] )){
-                        if((json[i+1]['label'] && json[i]['label']!=json[i+1]['label'] )|| (json[i]['hold']=="是" && json[i+1]['hold']=='是')){
+                        if((json[i+1]['label'] && json[i]['label']!=json[i+1]['label'] )|| (json[i]['hold']=="是" || json[i+1]['hold']=='是')){
                             var xlsmarks = (!json[i]['marks'])?"":json[i]['marks'];
 
                             j=j+1;
