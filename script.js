a48dcb5c5878f77ebd6d5231053137cd9b36f620
Xun <xun.li@student.maastrichtuniversity.nl>
2024-08-19 17:59:54 +0200

    up

--- script.js
+++ script.js
@@ -104,12 +104,16 @@ function login(){
 
 function searchjobs(searchcreteria){
     showloading(document.getElementById("activejobs"));
-    
+    const actionToken = Symbol();
+    latestActionToken = actionToken;
     const xhr  = new XMLHttpRequest();  
     xhr.open("POST", "https://garfat.xyz/index.php/home/Wms/searchjobs", true);
     xhr.onreadystatechange= () => {
         if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
             if(xhr.response["error_code"]==0){
+                if (actionToken !== latestActionToken) {
+                    return;
+                }
                 document.getElementById("activejobs").innerHTML="";
                 
                 searchedjobs=xhr.response["data"];
@@ -761,6 +765,8 @@ function printSpecificContent(clickeditem) {
 
 async function showinventory(searchcreteria){
     showloading(document.getElementById("activejobs"));
+    const actionToken = Symbol();
+    latestActionToken = actionToken;
     const showinventorymap = document.getElementById('inventorymapbutton');
     showinventorymap.disabled = true;
     
@@ -770,6 +776,9 @@ async function showinventory(searchcreteria){
       });
 
     const data = await response.json();
+    if (actionToken !== latestActionToken) {
+        return;
+    }
     searchedinventory = data['data'];
     showinventorymap.disabled = false;
     var activejobs = document.getElementById("activejobs");
@@ -830,13 +839,17 @@ async function showinventory(searchcreteria){
 }
 async function showitems(searchcreteria){
     showloading(document.getElementById("activejobs"));
+    const actionToken = Symbol();
+    latestActionToken = actionToken;
     const response = await fetch('https://garfat.xyz/index.php/home/Wms/searchitems', {
         method: 'POST',
         body: searchcreteria,
       });
 
     const data = await response.json();
-    
+    if (actionToken !== latestActionToken) {
+        return;
+    }
     
     var activejobs = document.getElementById("activejobs");
     activejobs.innerHTML="";
