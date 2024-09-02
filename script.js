2f2981e38a2eaa2a461a58be94c3ed1ace42b110
Xun <xun.li@student.maastrichtuniversity.nl>
2024-09-02 22:42:21 +0200

    up

--- script.js
+++ script.js
@@ -670,6 +670,8 @@ function showitemsearchbox(){
     exportbutton.disabled = true;
     reportform.appendChild(exportbutton);
 
+    reportform.appendChild(createTooltip('导出CSV后，请使用Excel中的“数据”->“从文本/CSV导入”功能打开，之后使用“数据到列”将数据分列。'));
+
     // Append form to body or any other container
     searchbox.appendChild(form);
     searchbox.appendChild(reportform);
