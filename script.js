092af8b07651936fdc25db3e7c743f9db77eea67
Xun <xun.li@student.maastrichtuniversity.nl>
2024-10-02 16:56:11 +0200

    drag

--- script.js
+++ script.js
@@ -1715,6 +1715,47 @@ async function loaddetail(clickeditem,activity,thisjobdiv,newadded){
             // }
             detaillinenumber=detaillinenumber+items.length;
         }
+
+        //make line form draggable
+        const draggables = document.querySelectorAll('.detaillineform');
+        const container = document.querySelector('.container');
+
+        draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', () => {
+            draggable.classList.add('dragging');
+        });
+
+        draggable.addEventListener('dragend', () => {
+            draggable.classList.remove('dragging');
+        });
+        });
+
+        container.addEventListener('dragover', e => {
+        e.preventDefault();
+        const afterElement = getDragAfterElement(container, e.clientY);
+        const dragging = document.querySelector('.dragging');
+        if (afterElement == null) {
+            container.appendChild(dragging);
+        } else {
+            container.insertBefore(dragging, afterElement);
+        }
+        });
+
+        function getDragAfterElement(container, y) {
+        const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
+
+        return draggableElements.reduce((closest, child) => {
+            const box = child.getBoundingClientRect();
+            const offset = y - box.top - box.height / 2;
+            if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child };
+            } else {
+            return closest;
+            }
+        }, { offset: Number.NEGATIVE_INFINITY }).element;
+        }
+
+        //stop here
     }
     
 
