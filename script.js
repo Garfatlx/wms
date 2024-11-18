94fd5dcb48bafb8ea00e5f7a8fc5ab6103d77a4f
Xun <xun.li@student.maastrichtuniversity.nl>
2024-11-18 10:26:47 +0100

    up

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ window.addEventListener("load", function(){
     
     access=-1;
     sysresponse = document.getElementById("response");
-    sysresponse.innerHTML="欢迎。近期更新频繁，建议每天第一次使用前按键盘Shift+F5刷新页面。v1.1.08";
+    sysresponse.innerHTML="欢迎。近期更新频繁，建议每天第一次使用前按键盘Shift+F5刷新页面。v1.1.09";
     
     //page fist load
     // var searchcreteria = new FormData();
