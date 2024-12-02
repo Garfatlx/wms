2254c2b22f6ac46d9f1ea3934b3e4fe636c006c5
Xun <xun.li@student.maastrichtuniversity.nl>
2024-12-02 10:47:38 +0100

    up

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ window.addEventListener("load", function(){
     
     access=-1;
     sysresponse = document.getElementById("response");
-    sysresponse.innerHTML="欢迎。近期更新频繁，建议每天第一次使用前按键盘Shift+F5刷新页面。v1.1.10";
+    sysresponse.innerHTML="欢迎。近期更新频繁，建议每天第一次使用前按键盘Shift+F5刷新页面。v1.1.12";
     
     //page fist load
     // var searchcreteria = new FormData();
@@ -5899,7 +5899,7 @@ function getemailaddress(customer){
         return 'garfat@live.com';
     }
     if(customer=='佳成'){
-        return null;
+        return "mcck_cz@jcex.com";
     }
     return null;
 }
